3f32dcc097fd65417485bff4d5d53b420e70ba0d
Antony Sajesh <antony@Antonys-MacBook-Air.local>
2025-04-08 11:37:32 -0500

    Day 2 commits

--- bank.py
+++ bank.py
@@ -3,6 +3,11 @@ import threading
 NUM_TELLERS = 3
 NUM_CUSTOMERS = 50
 
+tellers_semaphore = threading.Semaphore(NUM_TELLERS)
+
+tellers_ready = []
+tellers_lock = threading.Lock()
+
 class Teller(threading.Thread):
     def __init__(self, teller_id):
         super().__init__()
@@ -10,6 +15,8 @@ class Teller(threading.Thread):
 
     def run(self):
         print(f"Teller {self.teller_id} ready to serve")
+        with tellers_lock:
+            tellers_ready.append(self)
 
 class Customer(threading.Thread):
     def __init__(self, customer_id):
@@ -17,7 +24,11 @@ class Customer(threading.Thread):
         self.customer_id = customer_id
 
     def run(self):
-        print(f"Customer {self.customer_id} wants to perform a transaction")
+        tellers_semaphore.acquire()
+        with tellers_lock:
+            teller = tellers_ready.pop(0)
+        print(f"Customer {self.customer_id} selects Teller {teller.teller_id}")
+        tellers_semaphore.release()
 
 for i in range(NUM_TELLERS):
     Teller(i).start()
